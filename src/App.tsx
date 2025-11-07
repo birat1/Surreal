995523b3d8d6620171618745b404bf9d2267f9f7
ba01081 <93601583+birat1@users.noreply.github.com>
2025-11-07 12:41:49 +0000

    Updated pathing to components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import Home from "./components/Home";
-import LoginPage from "./components/LoginPage";
-import ProtectedRoute from "./components/ProtectedRoute";
-import SignUpPage from "./components/SignUpPage";
-import UserProfilePage from "./components/UserProfileSetupPage";
+import Home from "@/components/Home";
+import LoginPage from "@/components/LoginPage";
+import ProtectedRoute from "@/components/ProtectedRoute";
+import SignUpPage from "@/components/SignUpPage";
+import UserProfilePage from "@/components/UserProfileSetupPage";
 
 import "./index.css"
 

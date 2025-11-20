1d11a6dfca9e2941bb6b8e637672e6d0924afd1d
ba01081 <93601583+birat1@users.noreply.github.com>
2025-11-20 01:35:32 +0000

    Implemented alembic for models

--- app/main.py
+++ app/main.py
@@ -10,8 +10,8 @@ from app.routers import messages, ws
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
+    # async with engine.begin() as conn:
+    #    await conn.run_sync(Base.metadata.create_all)
     yield
 
 app = FastAPI(lifespan=lifespan)

477198a29d5a4b28f8cbb1ddb305dd6796ed86d9
ba01081 <93601583+birat1@users.noreply.github.com>
2025-11-20 14:06:32 +0000

    Updated ports

--- app/config.py
+++ app/config.py
@@ -1,8 +1,7 @@
 # CORS settings
 ALLOWED_ORIGINS = [
-    "http://127.0.0.1:5500",
-    "http://localhost:8000",
-    "http://127.0.0.1:8000",
+    "http://localhost:8001",
+    "http://127.0.0.1:8001",
 ]
 
 # testing users

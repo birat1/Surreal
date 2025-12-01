5683e0f8d251cc6c40c4f0c4ab539ec0ca2506df
ba01081 <93601583+birat1@users.noreply.github.com>
2025-12-01 18:40:06 +0000

    Fixed Spelling

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
             <Route
               path="/events-and-societies"
               element={
-                <EventsAndSocietiesPagePage />
+                <EventsAndSocietiesPage />
               }
             />
           </Routes>

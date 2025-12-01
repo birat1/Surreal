8f3764ae35d95296d06511ee4a844b9c6667b755
ba01081 <93601583+birat1@users.noreply.github.com>
2025-12-01 18:12:07 +0000

    Created frontend for Messages Page

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import LoginPage from "@/components/LoginPage";
 import ProtectedRoute from "@/routes/ProtectedRoute";
 import SignUpPage from "@/components/SignUpPage";
 import UserProfilePage from "@/components/UserProfileSetupPage";
-import FriendsFinderPage from "./components/FriendsFinderPage";
-import Navbar from "./components/Navbar";
+import FriendsFinderPage from "@/components/FriendsFinderPage";
+import MessagesPage from "@/components/MessagesPage";
+import Navbar from "@/components/Navbar";
 
 import "./index.css";
 
@@ -51,6 +52,12 @@ function App() {
                 <ProtectedRoute>
                   <FriendsFinderPage />
                 </ProtectedRoute>
+              }
+            />
+            <Route
+              path="/messages"
+              element={
+                <MessagesPage />
               }
             />
           </Routes>

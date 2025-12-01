f4bbd99d71a4a5f8a57151fd58722ea907c87757
as04877 <as04877@surrey.ac.uk>
2025-12-01 18:34:10 +0000

    Route ammendment

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ function App() {
             <Route
               path="/events-and-societies"
               element={
-                <MessagesPage />
+                <EventsAndSocietiesPagePage />
               }
             />
           </Routes>

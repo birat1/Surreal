f5c1dc8c8944b186c5c8a1c0b8f2d63920d995ad
as04877 <as04877@surrey.ac.uk>
2025-12-01 18:28:27 +0000

    Amendments to route

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import SignUpPage from "@/components/SignUpPage";
 import UserProfilePage from "@/components/UserProfileSetupPage";
 import FriendsFinderPage from "@/components/FriendsFinderPage";
 import MessagesPage from "@/components/MessagesPage";
+import EventsAndSocietiesPage from "@/components/EventsAndSocietiesPage";
 import Navbar from "@/components/Navbar";
 
 import "./index.css";

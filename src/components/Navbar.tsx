e7601ea16a5e8c7c85117807cf1a126c1cf0308b
Yuken <72272663+AnthemColours@users.noreply.github.com>
2025-12-14 20:58:42 +0000

    Added dropdown to notification bell

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,7 @@ import surreyLogo from '../assets/surrey_logo.jpg';
 
 import NotificationBell from './NotificationBell';
 import { Button } from './ui/button';
+import { NotificationProvider } from '@/context/NotificationContext';
 
 
 const Navbar: React.FC = () => {
@@ -56,7 +57,9 @@ const Navbar: React.FC = () => {
                         {isAuthenticated ? (
                             // If logged in → show Logout
                             <>
-                                <NotificationBell />
+                                <NotificationProvider>
+                                    <NotificationBell />
+                                </NotificationProvider>
                                 <Button
                                     variant="destructive"
                                     onClick={handleLogout}

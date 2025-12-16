eb9e897dd57c556df2ef925cb330d36d96653291
as04877 <as04877@surrey.ac.uk>
2025-12-16 01:17:34 +0000

    Create Event Button functional (minus Admin restriction)

--- src/pages/EventsAndSocietiesPage.tsx
+++ src/pages/EventsAndSocietiesPage.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import EventsCard from "@/components/EventsCard";
 import type { Event } from "@/types/types";
+import { useNavigate } from 'react-router-dom';
 
 const EventsAndSocietiesPage = () => {
   const [events, setEvents] = useState<Event[]>([]);
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetch("http://localhost:8000/events")
@@ -20,6 +22,11 @@ const EventsAndSocietiesPage = () => {
           <EventsCard key={index} event={event} />
         ))}
       </div>
+      <button
+          onClick={() => navigate('/create-event')}
+          className="mt-6 px-4 py-2 bg-blue-500 text-white rounded">
+          Create Event
+        </button>
     </div>
   );
 };
